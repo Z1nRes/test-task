1565d35b7799f4fb6833e4af93e50c3299900c2d
Z1n <tv.noob@bk.ru>
2024-01-10 00:04:16 +0700

    fix1

--- js/custom.js
+++ js/custom.js
@@ -2,22 +2,45 @@
 btnEquity = document.querySelector('.equity-content-btn')
 btnEarlyAccess = document.querySelector('.get-early-access-btn')
 btnBlog = document.querySelector('.news-btn')
-btnContact = document.querySelector('.contact-form-btn')
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    
+    modalWindow.classList.remove('hidden')
+    modalWindow.classList.add('d-flex')
+
+    formModal.classList.remove('d-block')
+    formModal.classList.add('hidden')
+
+    successModal.classList.remove('hidden')
+    successModal.classList.add('d-flex')
+
+    event.target.reset(); 
+}
+
+const applicantForm = Object.values(document.getElementsByClassName('form'))
+
+for (let form in applicantForm){
+    applicantForm[form].addEventListener('submit', handleFormSubmit)
+}
 
 modalWindow = document.querySelector('.modal-window')
 successModal = document.querySelector('.success-modal')
 formModal = document.querySelector('.modal-signup')
 
-modalWindow.addEventListener("click", () => {
-    modalWindow.classList.remove('d-flex')
-    modalWindow.classList.add('hidden')
-    successModal.classList.remove('d-flex')
-    successModal.classList.add('hidden')
-    formModal.classList.remove('d-block')
-    formModal.classList.add('hidden')
+modalWindow.addEventListener("click", ({target}) => {
+    if (target.classList.contains('modal-window')) {
+        modalWindow.classList.remove('d-flex')
+        modalWindow.classList.add('hidden')
+        successModal.classList.remove('d-flex')
+        successModal.classList.add('hidden')
+        formModal.classList.remove('d-block')
+        formModal.classList.add('hidden')
+    }
 })
 
 btnEquity.addEventListener("click", () => {
+    
     modalWindow.classList.remove('hidden')
     modalWindow.classList.add('d-flex')
 
@@ -25,23 +48,7 @@ btnEquity.addEventListener("click", () => {
     formModal.classList.add('d-block')
 })
 
-btnEarlyAccess.addEventListener("click", () => {
-    modalWindow.classList.remove('hidden')
-    modalWindow.classList.add('d-flex')
-
-    successModal.classList.remove('hidden')
-    successModal.classList.add('d-flex')
-})
-
 btnBlog.addEventListener("click", () => {
-    modalWindow.classList.remove('hidden')
-    modalWindow.classList.add('d-flex')
-
-    successModal.classList.remove('hidden')
-    successModal.classList.add('d-flex')
-})
-
-btnContact.addEventListener("click", () => {
     modalWindow.classList.remove('hidden')
     modalWindow.classList.add('d-flex')
 
